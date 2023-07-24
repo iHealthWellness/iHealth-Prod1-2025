e24d43da8d24e8dc70584ed195f435488905152d
fiiwebarry <fiiwebarry@yahoo.com>
2023-07-24 19:53:22 +0100

    update on page

--- src/Pages/About-us/index.jsx
+++ src/Pages/About-us/index.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import "./index.css";
+import Hero from "src/Components/About-Page-Layout/Hero";
+import Statement from "src/Components/About-Page-Layout/Statement";
+import Finance from "src/Components/About-Page-Layout/Finance";
+import Terms from "src/Components/About-Page-Layout/Terms";
+import TotopBtn from "src/Components/About-Page-Layout/TotopBtn";
+
+
+
+
+
+
 
 const index = () => {
-  return <div>index</div>;
+  return (
+    <>
+
+      <Hero />
+      <Statement />
+      <Finance />
+      <Terms />
+      <TotopBtn />
+
+
+
+    </>
+
+  )
+
 };
 
 export default index;
