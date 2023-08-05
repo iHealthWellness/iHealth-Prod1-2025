b99f6472a6d32ba10ec44ec361127274839d9153
fiiwebarry <fiiwebarry@yahoo.com>
2023-08-05 19:05:29 +0100

    update on About-us

--- src/Components/About-Page-Layout/Hero/index.jsx
+++ src/Components/About-Page-Layout/Hero/index.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import "./index.css";
 
-import img from "src/Assets/Images/business-meeting.png";
+import img from "src/Assets/Images/business-meeting2.png";
 
 const Hero = () => {
 
 
     return (
         <section className="Hero-image">
-            <div className="container"> <img src={img} className="img-holder" alt="meeting" /></div>
+            <div className="container"> <div className="image-page">
+                <img src={img} className="img-holder" alt="meeting" />
+            </div>
+
+            </div>
 
 
         </section>
--- src/Components/Team-Layout/Board/Board.jsx
+++ src/Components/Team-Layout/Board/Board.jsx
@@ -1,3 +1,7 @@
+import Image from "src/Assets/Images/sasika.png";
+import Image1 from "src/Assets/Images/Jeff.png";
+import Image2 from "src/Assets/Images/Mathias.png";
+import Image3 from "src/Assets/Images/Joseph.png";
 
 import "./Board.css";
 const Board = () => {
@@ -11,7 +15,29 @@ const Board = () => {
 
 
                     <h3 className="team-directors-tab">Our Board of Directors governs the Foundation's mission, policies, bylaws, budget, and fundraising initiatives,
-                        aligning with legal requirements and the purposes outlined in the Certificate of Incorporation and the Bylaws.</h3></div>
+                        aligning with legal requirements and the purposes outlined in the Certificate of Incorporation and the Bylaws.</h3>
+                </div>
+
+                <div className="board-image">
+                    <div className="board-directors-tab">
+                        <img src={Image} alt="" />
+                        <img src={Image1} alt="" />
+                    </div>
+                    <div className="board-directors-tab">
+                        <img src={Image2} alt="" />
+                        <img src={Image3} alt="" />
+                    </div>
+
+                </div>
+                <div>
+                    <h2 className="team-directors-header">Advisory Council</h2>
+                    <h3 className="team-directors-tab"> The Advisory Committee for our health and wellness startup serves as a group
+                        of experienced advisors who provide valuable insights, guidance, and expertise.
+                        They contribute to strategic decision-making, offer industry-specific knowledge,
+                        and provide recommendations on various aspects of our business, helping us drive
+                        innovation and ensure the effectiveness of our programs and services.</h3>
+                </div>
+
 
             </div>
         </div >
