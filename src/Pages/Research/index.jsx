9a20bf693c8f16432e8e184b1fcb53a6db7b1593
onehealthconnectapp <126990008+onehealthconnectapp@users.noreply.github.com>
2023-08-10 17:37:43 -0400

    Revert "update on research page"

--- src/Pages/Research/index.jsx
+++ src/Pages/Research/index.jsx
@@ -1,30 +1,7 @@
 import React from "react";
 
-import Hero from "src/Components/Research-page/Hero";
-import Tools from "src/Components/Research-page/Research tools";
-import Therapies from "src/Components/Research-page/Therapies";
-import Moreinfo from "src/Components/Research-page/More-info";
-import Segas from "src/Components/Research-page/Segas";
-import Participate from "src/Components/Research-page/Participate";
-import Involvement from "src/Components/Research-page/Involvement";
-import Consideration from "src/Components/Research-page/Consideration";
-import ToTopBtn from "src/Components/Research-page/TotopBtn";
-import"./index.css"
-
 const index = () => {
-  return (
-    <div className="research-page">
-      <Hero />
-      <Tools />
-      <Therapies />
-      <Moreinfo />
-      <Segas />
-      <Participate />
-      <Involvement />
-      <Consideration />
-      <ToTopBtn />
-    </div>
-  );
+  return <div>index</div>;
 };
 
 export default index;
