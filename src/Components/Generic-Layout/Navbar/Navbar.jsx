8a10a32a70692ca9c45f030c9ccacf79484762cb
onehealthconnectapp <126990008+onehealthconnectapp@users.noreply.github.com>
2023-08-13 15:54:26 -0400

    Revert "7/24/23 - Prod1 codebase copied to DEV1"

--- src/Components/Generic-Layout/Navbar/Navbar.jsx
+++ src/Components/Generic-Layout/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { NavLink, Link } from "react-router-dom";
-
+import { NavLink } from "react-router-dom";
 
 import NavDropdownButton from "./NavDropdown";
 import desktopLogo from "src/Assets/Brand/logo-nav-desktop.png";
@@ -28,63 +27,59 @@ const Navbar = () => {
           </div>
 
           <div className="nav-links-block">
-            {/* Desktop */}
             <ul className="nav-links-list desktop">
               <li className="nav-links-listitem">
                 <NavLink to="/" className="nav-links nav-home">
                   Home
                 </NavLink>
               </li>
-              <li className="nav-links-listitem nav-dropdown">
-                <NavLink to="/services" className="nav-links nav-services">
+              <li className="nav-links-listitem">
+                <NavLink
+                  onClick={() => {
+                    document.querySelector(
+                      "#UnderConst-wrapper"
+                    ).style.display = "flex";
+                  }}
+                  className="nav-links nav-services"
+                >
                   Services
                 </NavLink>
-                <div className="nav-dropdown-content">
-                  <a href="#">Stories of Strength</a>
-                  <a href="#">Share Your Story</a>
-                  <a href="#">Create Your Blog</a>
-                </div>
               </li>
-
-              {/* about us */}
-              <li className="nav-links-listitem nav-dropdown">
-                <NavLink to="/about-us" className="nav-links nav-about-us">
+              <li className="nav-links-listitem">
+                <NavLink
+                  onClick={() => {
+                    document.querySelector(
+                      "#UnderConst-wrapper"
+                    ).style.display = "flex";
+                  }}
+                  className="nav-links nav-about-us"
+                >
                   About Us
                 </NavLink>
-                <div className="nav-dropdown-content">
-                  {/* <NavLink to="/about-us/#Hero" >Corporate Info </NavLink>
-                  <NavLink to="/about-us/#Statement">Our Team </NavLink>
-                  <NavLink to="/about-us/#Finance">Financial Transparency </NavLink>
-                  <NavLink to="/about-us/#Terms">Terms Of Use</NavLink> */}
-
-                  <a href="/about-us/#Hero">Corporate Info </a>
-                  <a href="/our-team">Our Team </a>
-                  <a href="/about-us/#Finance">Financial Transparency </a>
-                  <a href="#Terms">Terms Of Use</a>
-                  <a href="#Statement">List Of Openings</a>
-
-
-
-                </div>
               </li>
-
-              {/* Research */}
-              <li className="nav-links-listitem nav-dropdown">
-                <NavLink to="/" className="nav-links nav-research">
+              <li className="nav-links-listitem">
+                <NavLink
+                  onClick={() => {
+                    document.querySelector(
+                      "#UnderConst-wrapper"
+                    ).style.display = "flex";
+                  }}
+                  className="nav-links nav-research"
+                >
                   Research
                 </NavLink>
-                <div className="nav-dropdown-content ">
-                  <a href="#">Research Tools & Resources</a>
-                  <a href="#">Clinical Drug Therapies</a>
-                  <a href="#">Participate in Research</a>
-                </div>
               </li>
             </ul>
-
-            {/* Sign up & sign in */}
             <ul className="nav-links-list">
-              <li className="nav-links-listitem nav-dropdown">
-                <NavLink className="nav-links nav-signin">
+              <li className="nav-links-listitem">
+                <NavLink
+                  onClick={() => {
+                    document.querySelector(
+                      "#UnderConst-wrapper"
+                    ).style.display = "flex";
+                  }}
+                  className="nav-links nav-signin"
+                >
                   <AccountCircleIcon
                     className="circle-icon"
                     sx={{ fontSize: 32, marginLeft: 0 }}
@@ -92,26 +87,18 @@ const Navbar = () => {
                   />
                   <span className="nav-signin-span">Sign up or Log In</span>
                 </NavLink>
-                <div className="nav-dropdown-content ">
-                  <a href="#">Sign In</a>
-                  <a href="#">Sign Up Now</a>
-                  <a href="#">Donate as a Guest</a>
-                </div>
               </li>
-
-              {/* Donate Link */}
-              <li className="nav-links-listitem nav-dropdown">
-                <NavLink className="nav-links nav-donate ">Donate</NavLink>
-                <div className="nav-dropdown-content ">
-                  <Link to="/donate">Ways to give</Link>
-                  <a href="#">Donate Online</a>
-                  <a href="#">Individual Donation</a>
-                  <a href="#">Make a Corporate Donation</a>
-                  <a href="#">Fundraise on Facebook</a>
-                  <a href="#">Create Your Own Fundraiser</a>
-                  <hr />
-                  <a href="#"> Raise $500 in 10 days</a>
-                </div>
+              <li className="nav-links-listitem">
+                <NavLink
+                  onClick={() => {
+                    document.querySelector(
+                      "#UnderConst-wrapper"
+                    ).style.display = "flex";
+                  }}
+                  className="nav-links nav-donate"
+                >
+                  Donate
+                </NavLink>
               </li>
             </ul>
           </div>
@@ -122,9 +109,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
-// onClick={() => {
-//   document.querySelector(
-//     "#UnderConst-wrapper"
-//   ).style.display = "flex";
-// }}
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 //Import Libraries/Packages
 import React from "react";
 import ReactDOM from "react-dom/client";
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // Import the AOS styles
-
 
 //Import Local files/components
 import App from "./App.jsx";
 
-AOS.init(); // Initialize AOS
+
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
