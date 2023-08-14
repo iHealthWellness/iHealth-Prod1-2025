6da201eeae4c6efd1595b38f783bb325564ca79e
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-08-14 22:47:39 +0100

    adding terms of use, geriatric and cancer form popup

--- src/Components/Terms-of-use/Topic-section/Termstopic.jsx
+++ src/Components/Terms-of-use/Topic-section/Termstopic.jsx
@@ -1,6 +1,6 @@
 import "./Termstopic.css";
 import communityLogo from "../../../Assets/Images/logo1.svg";
-import plusicon from "../../../Assets/Icons/Plus-icon.png";
+import plusicon from "../../../Assets/Icons/plus-icon.png";
 import minusicon from "../../../Assets/Icons/minus-icon.png";
 // fontawesome
 // import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
