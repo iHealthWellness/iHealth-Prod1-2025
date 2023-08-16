328e04971a81e73b3e4054559f4d6bf514810b5a
fiiwebarry <fiiwebarry@yahoo.com>
2023-08-16 17:38:24 +0100

    update

--- src/Components/About-Page-Layout/Hero/index.jsx
+++ src/Components/About-Page-Layout/Hero/index.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
 import "./index.css";
 
-import img from "src/Assets/Images/business-meeting.png";
+import img from "src/Assets/Images/business-meeting3.png";
 
 const Hero = () => {
 
 
     return (
-        <section  id="Hero" className="Hero-image">
+        <section id="Hero" className="Hero-image">
             <div className="container"> <div className="image-page">
                 <img src={img} className="img-holder" alt="meeting" />
             </div>
