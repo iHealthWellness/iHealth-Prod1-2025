01f33e4692d7cc670ac8d317cff201d4ede42a17
kelvin <knjuiri@gmail.com>
2023-08-20 19:48:10 +0300

    update

--- src/Components/Terms-of-use/Hero/Hero.jsx
+++ src/Components/Terms-of-use/Hero/Hero.jsx
@@ -5,17 +5,11 @@ const Hero = () => {
   return (
     // Hero image on terms of use page
     <>
-<<<<<<< HEAD
       <div className="terms0">
-=======
-<<<<<<< HEAD
-      <div id="termsofuse">
-=======
       <div className="terms0">
->>>>>>> 8786c76 (adding getriatric and cancer form popup)
->>>>>>> ff6df77 (update on the page)
         <img src={termsOfuse} alt="terms-of-use" />
       </div>
+    </div>
     </>
   );
 };
