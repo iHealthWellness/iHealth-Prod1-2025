3af24e35bacabc8fe51312fc76e01b04486ec7cb
AmalStudy <amalengstudy@gmail.com>
2023-08-30 18:30:10 -0700

    fixed aboute page

--- src/Components/Team-Layout/DataPage/DataPage.jsx
+++ src/Components/Team-Layout/DataPage/DataPage.jsx
@@ -5,84 +5,84 @@ const DataPage = [
     {
         id: 1,
         TeamName: "Copywriting",
-        Roles: "Content Writers, Copywriters, Technical Writers",
+        // Roles: "Content Writers, Copywriters, Technical Writers",
         Names: ["Fizza Haider"],
 
     },
     {
         id: 2,
         TeamName: "Data Analysis ",
-        Roles: "Data Analysts,Data Scientists,Data Engineers ",
+        // Roles: "Data Analysts,Data Scientists,Data Engineers ",
         Names: ["Ajeyomi Adedoyin", "Alim Nuriiev", "Ayushi Malaviya", "Ellen Fernandez", "Emmanuel Momoh", "Maaz Ghazi", "Manish Sharma"],
 
     },
     {
         id: 3,
         TeamName: " Design",
-        Roles: "UX/UI Designers, Interaction Designers, Visual Designers, Graphic Designers ",
+        // Roles: "UX/UI Designers, Interaction Designers, Visual Designers, Graphic Designers ",
         Names: [" Adel Akhmed", "Camilla Menin", "Elysa Gaona", "Fiona Doyle", "Hanqi Li", "Innocent Opara", "Justina Ackah", "Mariam Abdulkareem","William Vo"],
 
     },
     {
         id: 4,
         TeamName: "Development",
-        Roles: " Software Engineers, Full-Stack Developers,Backend Developers,Frontend Developers, DevOps Engineers,Mobile App Developers ",
-        Names: ["Barisanem Fiiwe", "Cem Mehmet Onal", "Emmanuel Adewoye", "Kelvin Njuiri", "Kiran Yadav", "Oladimeji Ibitoye", "Takuzen LU", "Udemeobong Akpan", "Xuelan Wu", "Yunus Olgan", "Zaid Rafi"],
+        // Roles: " Software Engineers, Full-Stack Developers,Backend Developers,Frontend Developers, DevOps Engineers,Mobile App Developers ",
+        Names: ["Amal Engulatov","Barisanem Fiiwe", "Cem Mehmet Onal", "Emmanuel Adewoye", "Kelvin Njuiri", "Kiran Yadav", "Oladimeji Ibitoye", "Takuzen LU", "Udemeobong Akpan", "Xuelan Wu", "Yunus Olgan", "Zaid Rafi"],
 
     },
     {
         id: 5,
         TeamName: "Financial Management  ",
-        Roles: "Financial Specialists, Financial Analysts  ",
+        // Roles: "Financial Specialists, Financial Analysts  ",
         Names: [" Matías Berisso", "Matt Zhang", "Mulhoteni Gomani"],
 
     },
     {
         id: 6,
         TeamName: " Human Resources",
-        Roles: "HR Managers, HR Generalists, Talent Acquisition Specialists",
+        // Roles: "HR Managers, HR Generalists, Talent Acquisition Specialists",
         Names: ["Marissa Flanagan", "Serena Pasqualini","Lucia Valerio"],
 
     },
     {
         id: 7,
         TeamName: " Legal and Compliance",
-        Roles: "General Counsel, Compliance Officer,Legal Counsel,Regulatory Affairs  Manager,Risk and Compliance Analyst,Privacy Officer, Ethics Officer, Contract Manager,Intellectual Property (IP) Manager,Compliance Training Specialist  ",
+        // Roles: "General Counsel, Compliance Officer,Legal Counsel,Regulatory Affairs  Manager,Risk and Compliance Analyst,Privacy Officer, Ethics Officer, Contract Manager,Intellectual Property (IP) Manager,Compliance Training Specialist  ",
         Names: ["Dalia Hechi"],
 
     },
     {
         id: 8,
         TeamName: "Marketing and Public Relations ",
-        Roles: "Marketing Managers,PR Specialists, Communications Managers  ",
+        // Roles: "Marketing Managers,PR Specialists, Communications Managers  ",
         Names: ["Nelson Izah", "Sean Banks"],
 
     },
     {
         id: 9,
         TeamName: "Product Management",
-        Roles: "Product Managers, Product Owners  ",
-        Names: ["Arsen Duzrapanyan", "Denis Kiura", "Hsuan Ouyang", "Melissa Enninful", "Morteza Mohammadi", "Susan Haynes", "Lisa Marquez"],
+        // Roles: "Product Managers, Product Owners  ",
+        Names: ["Arsen Duzrapanyan", "Denis Kiura", "Hsuan Ouyang", "Melissa Enninful", "Susan Haynes",],
 
     },
     {
         id: 10,
         TeamName: "Project Management ",
-        Roles: "Project Managers, Scrum Masters, Agile Coaches  ",
+        // Roles: "Project Managers, Scrum Masters, Agile Coaches  ",
         Names: ["Anu Ramabhadran", "Sherrie Ricks","Lynda Marsh"],
 
     },
     {
         id: 11,
         TeamName: "Quality Assurance ",
-        Roles: "QA Engineers, Testers, Quality Assurance Specialists  ",
+        // Roles: "QA Engineers, Testers, Quality Assurance Specialists  ",
         Names: ["Bhavitha Keezhuridathil", "Femi Gideon", "Kofi Tuffour", "Ossei Piyush", "Chakkarwar"],
 
     },
     {
         id: 12,
         TeamName: "Research and Innovation ",
-        Roles: " Research Scientists, Innovation Strategists, R&D Specialists ",
+        // Roles: " Research Scientists, Innovation Strategists, R&D Specialists ",
         Names: ["Marcella Sarti Souza MD"],
 
     },
