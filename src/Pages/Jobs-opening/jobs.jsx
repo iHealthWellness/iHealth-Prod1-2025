d79a6a5d158aab0fbe274dce9194c49fc771c335
kelvin <knjuiri@gmail.com>
2023-09-14 01:35:54 +0300

    update

--- src/Pages/Jobs-opening/jobs.jsx
+++ src/Pages/Jobs-opening/jobs.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
-import Hiring from "src/Components/Job-openings/Hiring/openings"
+import Hiring from "src/Components/Job-openings/Hiring/openings";
+import Copywrite from 'src/Components/Job-openings/Copywriting/copywrite';
+import Cybersecurity from 'src/Components/Job-openings/Cybesecurity/cybersecurity';
+import Data from 'src/Components/Job-openings/Data-science/data';
 
 const jobs = () => {
   return (
     <div>
         <Hiring/>
+        <Copywrite/>
+        <Cybersecurity/>
+        <Data/>
     </div>
   )
 }
