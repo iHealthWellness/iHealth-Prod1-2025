21c50da4c589ae5eecb0b89dbb5b45091f3afabe
fiiwebarry <fiiwebarry@yahoo.com>
2023-09-13 20:59:52 +0100

    update on page

--- src/Pages/Jobs-opening/jobs.jsx
+++ src/Pages/Jobs-opening/jobs.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import Header from 'src/Components/Job-openings/Header/Header';
+import Hiring from 'src/Components/Job-openings/Hiring/Hiring';
+// import openings from 'src/Components/Job-openings/Hiring/openings/openings';
 
-import Header from "src/Components/Job-openings/Header/Header";
-import openings from 'src/Components/Job-openings/Header/Hire/Hiring/openings';
+
 const jobs = () => {
   return (
     <div>
       <Header />
-      <openings />
+      <Hiring />
 
     </div>
   )
