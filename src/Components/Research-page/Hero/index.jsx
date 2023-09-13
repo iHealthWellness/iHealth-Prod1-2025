657923d4a0f3ab188c38c616b81f2e0826d94a21
Amal Engulatov <116880367+AmalEN20@users.noreply.github.com>
2023-09-13 14:57:40 -0700

    Update index.jsx

--- src/Components/Research-page/Hero/index.jsx
+++ src/Components/Research-page/Hero/index.jsx
@@ -42,12 +42,6 @@ const section1 = () => {
           </h1>
           <h2 className="side2">Advancing Research for a Brighter Future</h2>
         </div>
-// <<<<<<< amalweb
-//         {/* <div className="slide-in-text"> */}
-// =======
-// {/*SN         <div className="slide-in-text"> */}
-//             <div>
-// >>>>>>> main
         <div className="intro">
           <h3 className="introduction">Introduction</h3>
           <p className="welcome">
@@ -79,7 +73,6 @@ const section1 = () => {
             of life for NF patients worldwide.
           </p>
         </div>
-        {/* </div> */}
       </div>
     </div>
   );
