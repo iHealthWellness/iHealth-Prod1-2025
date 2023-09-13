cd0407f06cb0691c4151f903b1d058dccddeb452
onehealthconnectapp <126990008+onehealthconnectapp@users.noreply.github.com>
2023-09-13 15:03:04 -0400

    9/13-SN-Animation-Update index.jsx

--- src/Components/Research-page/Hero/index.jsx
+++ src/Components/Research-page/Hero/index.jsx
@@ -42,7 +42,8 @@ const section1 = () => {
           </h1>
           <h2 className="side2">Advancing Research for a Brighter Future</h2>
         </div>
-        <div className="slide-in-text">
+{/*SN         <div className="slide-in-text"> */}
+            <div>
         <div className="intro">
           <h3 className="introduction">Introduction</h3>
           <p className="welcome">
