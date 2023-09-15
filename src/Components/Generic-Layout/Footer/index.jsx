2c9cd81c64f8103920c9d132145a0a153b061e05
ephy-bayru <ephybayru@gmail.com>
2023-09-16 00:23:01 +0300

    feat: Integrate environment-based configuration and update API calls
    - Refactored the config.js to use Vite's import.meta.env for environment variables
    - Updated API services to use the new PROD_URL constant for making requests
    - Added environment variables for development and production in .env files
    - Logged the current running environment for debugging

--- src/Components/Generic-Layout/Footer/index.jsx
+++ src/Components/Generic-Layout/Footer/index.jsx
@@ -10,7 +10,7 @@ import FooterLink from "src/Constants/FooterLink.js";
 
 import "./index.css";
 import axios from "axios";
-import { PROD_URL } from '../../../../environment/config';
+import { PROD_URL } from '../../../environment/config';
 
 const Footer = () => {
   const [selectedLanguage, setSelectedLanguage] = useState("en");
--- src/config/urls.js
+++ src/config/urls.js
@@ -1,3 +1,3 @@
-import { PROD_URL } from '../../environment/config';
+import { PROD_URL } from '../environment/config';
 
 export const getProviders = `${PROD_URL}/api/v1/646a366608cb638735093bc7`;
