f5a17583727f89f290c3ddf368497e59beb410fa
kelvin <knjuiri@gmail.com>
2023-09-18 19:24:01 +0300

    openings update

--- src/Components/Job-openings/Hiring/IT.jsx
+++ src/Components/Job-openings/Hiring/IT.jsx
@@ -19,7 +19,6 @@ const It = () => {
     setAccordion((prev) => ({ ...prev, [content]: !prev[content] }));
   };
 
-
   return (
     <div className="layerTwo-tab">
       <div>
@@ -47,108 +46,107 @@ const It = () => {
 
           <hr />
           {accordion.sight && (
-          <div className="text-tab-flow">
-            <h2 className="description-title">Description</h2>
-            <p className="description-text">
-              We are an 'early stage' pre-money nonprofit/ tech startup
-              preparing for a proof-of-concept launch / seed-raise looking
-              to recruit VOLUNTEERS (unpaid) as SALESFORCE ARCHITECT (HEALTH
-              CLOUD IMPLEMENTATION EXPERT).You’re able to provide technical
-              solutions adhering to SFHC best practices.
-            </p>
-            <h3 className="responsibilities-title">Responsibilities</h3>
-            <ul className="responsibilities-text">
-              <li>Develop technology roadmaps to support business goals</li>
-              <li>
-                Provide technological expertise and advice to IT leadership
-                in the development of strategic information technology plans
-                to support business strategies
-              </li>
-              <li>
-                Prior experience implementing Salesforce solutions for
-                Healthcare Payers or Providers
-              </li>
-              <li>
-                Functional experience working with system integrations using
-                the API form within the Salesforce platform in addition to
-                integrations with other systems using middleware such as
-                Mulesoft
-              </li>
-              <li>
-                Health Cloud implementation, Salesforce Lightning,
-                Salesforce Configuration, Customization, Service Cloud
-                Development , Integration with 3rd party systems and custom
-                development
-              </li>
-              <li>
-                Assist in defining, estimating, and shaping technical
-                solutions using best practices
-              </li>
-              <li>
-                Support users & Business Analysts to troubleshoot
-                application issues and develop creative solutions to support
-                business needs
-              </li>
-              <li>
-                Lead and manage the design, configuration, and deployment
-              </li>
-              <li>
-                Monitor new Salesforce release features and functionality
-                and provide recommendations for process improvements
-              </li>
-            </ul>
-            <h3 className="qualifications-title">Qualifications</h3>
+            <div className="text-tab-flow">
+              <h2 className="description-title">Description</h2>
+              <p className="description-text">
+                We are an 'early stage' pre-money nonprofit/ tech startup
+                preparing for a proof-of-concept launch / seed-raise looking to
+                recruit VOLUNTEERS (unpaid) as SALESFORCE ARCHITECT (HEALTH
+                CLOUD IMPLEMENTATION EXPERT).You’re able to provide technical
+                solutions adhering to SFHC best practices.
+              </p>
+              <h3 className="responsibilities-title">Responsibilities</h3>
+              <ul className="responsibilities-text">
+                <li>Develop technology roadmaps to support business goals</li>
+                <li>
+                  Provide technological expertise and advice to IT leadership in
+                  the development of strategic information technology plans to
+                  support business strategies
+                </li>
+                <li>
+                  Prior experience implementing Salesforce solutions for
+                  Healthcare Payers or Providers
+                </li>
+                <li>
+                  Functional experience working with system integrations using
+                  the API form within the Salesforce platform in addition to
+                  integrations with other systems using middleware such as
+                  Mulesoft
+                </li>
+                <li>
+                  Health Cloud implementation, Salesforce Lightning, Salesforce
+                  Configuration, Customization, Service Cloud Development ,
+                  Integration with 3rd party systems and custom development
+                </li>
+                <li>
+                  Assist in defining, estimating, and shaping technical
+                  solutions using best practices
+                </li>
+                <li>
+                  Support users & Business Analysts to troubleshoot application
+                  issues and develop creative solutions to support business
+                  needs
+                </li>
+                <li>
+                  Lead and manage the design, configuration, and deployment
+                </li>
+                <li>
+                  Monitor new Salesforce release features and functionality and
+                  provide recommendations for process improvements
+                </li>
+              </ul>
+              <h3 className="qualifications-title">Qualifications</h3>
 
-            <ul className="qualifications-text">
-              <li>
-                Must have Experience in Salesforce Health Cloud as a
-                Salesforce Administrator*
-              </li>
-              <li>
-                Very good working functional and technical knowledge of at
-                least 6 Salesforce products e.g: Service Cloud, Health Cloud
-                , Experience Cloud etc.
-              </li>
+              <ul className="qualifications-text">
+                <li>
+                  Must have Experience in Salesforce Health Cloud as a
+                  Salesforce Administrator*
+                </li>
+                <li>
+                  Very good working functional and technical knowledge of at
+                  least 6 Salesforce products e.g: Service Cloud, Health Cloud ,
+                  Experience Cloud etc.
+                </li>
 
-              <li>
-                Experience in the US Healthcare System and Pharmaceutical
-                Patient Services
-              </li>
+                <li>
+                  Experience in the US Healthcare System and Pharmaceutical
+                  Patient Services
+                </li>
 
-              <li>
-                Experienced in writing technical documentation such as a
-                design document
-              </li>
-              <li>Well versed with Agile ways of working (Scrum)</li>
-              <li>
-                Experience in the US Healthcare System and Pharmaceutical
-                Patient Services
-              </li>
-              <li>Excellent communication skills</li>
-              <li>Familiarity with Agile frameworks is a plus</li>
-              <li>Ability to commit to a minimum of 4+ hour work weeks</li>
-              <li>
-                Ability to attend weekly standup meeting (conference call
-                with desktop/ laptop access for screen sharing)
-              </li>
-              <li>HealthTech experience is a plus</li>
-            </ul>
+                <li>
+                  Experienced in writing technical documentation such as a
+                  design document
+                </li>
+                <li>Well versed with Agile ways of working (Scrum)</li>
+                <li>
+                  Experience in the US Healthcare System and Pharmaceutical
+                  Patient Services
+                </li>
+                <li>Excellent communication skills</li>
+                <li>Familiarity with Agile frameworks is a plus</li>
+                <li>Ability to commit to a minimum of 4+ hour work weeks</li>
+                <li>
+                  Ability to attend weekly standup meeting (conference call with
+                  desktop/ laptop access for screen sharing)
+                </li>
+                <li>HealthTech experience is a plus</li>
+              </ul>
 
-            <p className="note-text">
-              Through your pro-bono work, you will not only continue to
-              demonstrate your current skills while gaining some new ones,
-              practice teamwork, make new friends and develop professional
-              connections in a professional setting, but you will also have
-              the opportunity to demonstrate what companies value in ways
-              other than paid employment: ability to get things done and
-              ability to work as a team.
-            </p>
+              <p className="note-text">
+                Through your pro-bono work, you will not only continue to
+                demonstrate your current skills while gaining some new ones,
+                practice teamwork, make new friends and develop professional
+                connections in a professional setting, but you will also have
+                the opportunity to demonstrate what companies value in ways
+                other than paid employment: ability to get things done and
+                ability to work as a team.
+              </p>
 
-            <p className="apply-text">
-              How to Apply: To apply for any open position, please contact
-              HR at onehealthconnectapp@gmail.com
-            </p>
-          </div>
+              <p className="apply-text">
+                How to Apply: To apply for any open position, please contact HR
+                at onehealthconnectapp@gmail.com
+              </p>
+            </div>
           )}
         </div>
       </div>
