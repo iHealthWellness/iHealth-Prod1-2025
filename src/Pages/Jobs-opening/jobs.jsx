0f63adbe81b8c8b32d0d1959e5fbd1c114766b82
kelvin <knjuiri@gmail.com>
2023-09-20 02:41:58 +0300

    update

--- src/Pages/Jobs-opening/jobs.jsx
+++ src/Pages/Jobs-opening/jobs.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import Header from 'src/Components/Job-openings/Header/Header';
 import Hiring from 'src/Components/Job-openings/Hiring/Hiring';
 // import openings from 'src/Components/Job-openings/Hiring/openings/openings';
