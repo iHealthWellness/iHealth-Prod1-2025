1f1bf315f8810814aa201b12332e4189bf5e723c
Emmanuel Adewoye <emmaxfans@gmail.com>
2023-11-18 23:41:22 +0100

    added the disease dropdown list to jc form

--- src/Components/Home-Page/Layout/Community/index.jsx
+++ src/Components/Home-Page/Layout/Community/index.jsx
@@ -1,97 +1,97 @@
 import { useState } from "react";
 
 import "./index.css";
-import Geriatric from "./Community-Team/Jc-team/Geriatric";
-import Cancer from "./Community-Team/Jc-team/Cancer";
+import Geriatric from "./Community-Team/Geriatric";
+import Cancer from "./Community-Team/Cancer";
 
 const Community = () => {
-	const [team, setTeam] = useState("NF TEAM");
+  const [team, setTeam] = useState("NF TEAM");
 
-	// Geriatric disesea team modal popup js
+  // Geriatric disesea team modal popup js
 
-	const toggleModal = () => {
-		setIsOpen(!isOpen);
-	};
-	const [isOpen, setIsOpen] = useState(false);
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
+  const [isOpen, setIsOpen] = useState(false);
 
-	// Cancer team modal popup js
+  // Cancer team modal popup js
 
-	const toggleModal1 = () => {
-		setIsOpens(!isOpens);
-	};
-	const [isOpens, setIsOpens] = useState(false);
+  const toggleModal1 = () => {
+    setIsOpens(!isOpens);
+  };
+  const [isOpens, setIsOpens] = useState(false);
 
-	const closeModal = () => {
-		setIsOpen(false);
-		setIsOpens(false);
-	};
+  const closeModal = () => {
+    setIsOpen(false);
+    setIsOpens(false);
+  };
 
-	return (
-		<section className="community-container">
-			<h2 className="community-heading">Empowering Health and Wellness</h2>
-			<p className="community-description">
-				Empower yourself with the information you need to make informed
-				decisions and lead a healthier, happier life. Choose your area of
-				interest and embark on a journey towards improved well-being.
-			</p>
+  return (
+    <section className="community-container">
+      <h2 className="community-heading">Empowering Health and Wellness</h2>
+      <p className="community-description">
+        Empower yourself with the information you need to make informed
+        decisions and lead a healthier, happier life. Choose your area of
+        interest and embark on a journey towards improved well-being.
+      </p>
 
-			<div className="community-teams">
-				<button
-					onClick={() => {
-						setTeam("NF TEAM");
-						document
-							.querySelector(".information-container")
-							.scrollIntoView({ behavior: "smooth" });
-						console.log(document.querySelector(".information-container"));
-					}}
-					className={
-						team === "NF TEAM"
-							? "community-team-btn focus"
-							: "community-team-btn"
-					}
-				>
-					Neurofibromatosis (NF) HUB
-				</button>
-				<button
-					// onClick={() => {
-					//   setTeam("Geriatric Disease Team");
-					//   document.querySelector("#UnderConst-wrapper").style.display =
-					//     "flex";
-					// }}
-					onClick={toggleModal}
-					className={
-						team === "Geriatric Disease Team"
-							? "community-team-btn focus"
-							: "community-team-btn"
-					}
-				>
-					Geriatric Disease Hub <p className="coming-soon-p">(Coming soon)</p>
-				</button>
-				<button
-					// onClick={() => {
-					//   setTeam("Cancer Team");
-					//   document.querySelector("#UnderConst-wrapper").style.display =
-					//     "flex";
-					// }}
-					onClick={toggleModal1}
-					className={
-						team === "Cancer Team"
-							? "community-team-btn focus"
-							: "community-team-btn"
-					}
-				>
-					Cancer Hub <p className="coming-soon-p">(Coming soon)</p>
-				</button>
-			</div>
-			{/* MODAL POPUP FOR GERIATRIC TEAM */}
+      <div className="community-teams">
+        <button
+          onClick={() => {
+            setTeam("NF TEAM");
+            document
+              .querySelector(".information-container")
+              .scrollIntoView({ behavior: "smooth" });
+            console.log(document.querySelector(".information-container"));
+          }}
+          className={
+            team === "NF TEAM"
+              ? "community-team-btn focus"
+              : "community-team-btn"
+          }
+        >
+          Neurofibromatosis (NF) HUB
+        </button>
+        <button
+          // onClick={() => {
+          //   setTeam("Geriatric Disease Team");
+          //   document.querySelector("#UnderConst-wrapper").style.display =
+          //     "flex";
+          // }}
+          onClick={toggleModal}
+          className={
+            team === "Geriatric Disease Team"
+              ? "community-team-btn focus"
+              : "community-team-btn"
+          }
+        >
+          Geriatric Disease Hub <p className="coming-soon-p">(Coming soon)</p>
+        </button>
+        <button
+          // onClick={() => {
+          //   setTeam("Cancer Team");
+          //   document.querySelector("#UnderConst-wrapper").style.display =
+          //     "flex";
+          // }}
+          onClick={toggleModal1}
+          className={
+            team === "Cancer Team"
+              ? "community-team-btn focus"
+              : "community-team-btn"
+          }
+        >
+          Cancer Hub <p className="coming-soon-p">(Coming soon)</p>
+        </button>
+      </div>
+      {/* MODAL POPUP FOR GERIATRIC TEAM */}
 
-			<div>{isOpen && <Geriatric closeModal={closeModal} />}</div>
+      <div>{isOpen && <Geriatric closeModal={closeModal} />}</div>
 
-			{/* MODAL POPUP FOR CANCER TEAM */}
+      {/* MODAL POPUP FOR CANCER TEAM */}
 
-			<div>{isOpens && <Cancer closeModal={closeModal} />}</div>
-		</section>
-	);
+      <div>{isOpens && <Cancer closeModal={closeModal} />}</div>
+    </section>
+  );
 };
 
 export default Community;
