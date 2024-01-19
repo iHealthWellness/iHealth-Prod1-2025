26881e39403ba686f1619f38c455a8337ec53ff5
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-01-19 19:03:54 +0400

    add third button in the heading

--- src/Components/LivingwithNF/Heading/index.jsx
+++ src/Components/LivingwithNF/Heading/index.jsx
@@ -8,18 +8,19 @@ function index() {
         <h1>Living with Neurofibromotisis</h1>
         <p className="nf-paragraph">
           A comprehensive web resource dedicated to providing support,
-          information,
-          <br /> and resources for individuals and families affected by
-          neurofibromatosis <br /> (NF). It servers as a hub for those
-          navigating the challenges associated with
-          <br /> NF, offering a wealth of valuable content.
+          information, and resources for individuals and families affected by
+          neurofibromatosis (NF). It servers as a hub for those navigating the
+          challenges associated with NF, offering a wealth of valuable content.
         </p>
         <div className="nf-btn-container">
           <Link to="#signup-reg">
             <button className="nf-btn">Sign-Up for Registry</button>
           </Link>
           <Link to="#take-survey">
-            <button className="nf-btn">Take the survey</button>
+            <button className="nf-btn">Survey Now</button>
+          </Link>
+          <Link to="#understand-nf">
+            <button className="nf-btn">Understand NF</button>
           </Link>
         </div>
       </div>
--- src/Pages/Living-with-NF/index.jsx
+++ src/Pages/Living-with-NF/index.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import CircleImages from 'src/Components/LivingwithNF/CircleImages/index'
-import NFHeading from 'src/Components/LivingwithNF/Heading/index'
-import  UnderstandingNF from 'src/Components/LivingwithNF/UnderstandingNF/index'
-import TabComponent from 'src/Components/LivingwithNF/UnderstandingNF/Tabcomponent/index'
-import CausesAspects from 'src/Components/LivingwithNF/Causes-Aspects/index'
-import SurveyNow from 'src/Components/LivingwithNF/Survey-Now/index'
-import SignupRegistry from 'src/Components/LivingwithNF/Signup-Registry/index'
+import React from "react";
+import CircleImages from "src/Components/LivingwithNF/CircleImages/index";
+import NFHeading from "src/Components/LivingwithNF/Heading/index";
+import UnderstandingNF from "src/Components/LivingwithNF/UnderstandingNF/index";
+import TabComponent from "src/Components/LivingwithNF/UnderstandingNF/Tabcomponent/index";
+import CausesAspects from "src/Components/LivingwithNF/Causes-Aspects/index";
+import SurveyNow from "src/Components/LivingwithNF/Survey-Now/index";
+import SignupRegistry from "src/Components/LivingwithNF/Signup-Registry/index";
 function index() {
   return (
     <div>
-        <NFHeading/>
-        <CircleImages/>
-        <UnderstandingNF/>
-        <TabComponent/>
-        <CausesAspects/>
-        <SurveyNow/>
-        <SignupRegistry/>
+      <NFHeading />
+      <CircleImages />
+      <UnderstandingNF />
+      <TabComponent />
+      <CausesAspects />
+      <SurveyNow />
+      <SignupRegistry />
     </div>
-  )
+  );
 }
 
-export default index
+export default index;
