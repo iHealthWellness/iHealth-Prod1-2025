4c752ccf567076d9cdc02d28d74398b8ebdec37d
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-01-20 17:32:58 +0400

    fix heading styling

--- src/Components/LivingwithNF/Heading/index.jsx
+++ src/Components/LivingwithNF/Heading/index.jsx
@@ -5,26 +5,27 @@ function index() {
   return (
     <div>
       <div className="heading-container">
-        <h1>Living with Neurofibromotisis</h1>
-        <p className="nf-paragraph">
+        <h1 className="SN-D-H1-48">Living with Neurofibromotisis (NF)</h1>
+        <p className="nf-paragraph SN-D-P-20">
           A comprehensive web resource dedicated to providing support,
           information, and resources for individuals and families affected by
-          neurofibromatosis (NF). It servers as a hub for those navigating the
+          neurofibromatosis (NF). It serves as a hub for those navigating the
           challenges associated with NF, offering a wealth of valuable content.
         </p>
         <div className="nf-btn-container">
           <Link to="#signup-reg">
-            <button className="nf-btn">Sign-Up for Registry</button>
+            <button className="nf-btn SN-D-T-M-B-16">
+              Sign-Up for Registry
+            </button>
           </Link>
-          <Link to="#take-survey">
-            <button className="nf-btn">Survey Now</button>
+          <Link to="#take-survey ">
+            <button className="nf-btn SN-D-T-M-B-16">Survey Now</button>
           </Link>
           <Link to="#understand-nf">
-            <button className="nf-btn">Understand NF</button>
+            <button className="nf-btn SN-D-T-M-B-16">Understanding NF</button>
           </Link>
         </div>
       </div>
-      <div className="horizontal-block"></div>
     </div>
   );
 }
