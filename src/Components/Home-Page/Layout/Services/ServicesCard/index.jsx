e131b3faec9f4042259eeb81196bc404ad576275
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-04-10 16:37:25 +0400

    change paddings and hover colors

--- src/Components/Home-Page/Layout/Services/ServicesCard/index.jsx
+++ src/Components/Home-Page/Layout/Services/ServicesCard/index.jsx
@@ -7,15 +7,15 @@ const ServicesCard = ({ service }) => {
       <div className="services-hover">
         <h3 className="services-card-heading">{service.title}</h3>
         <p className="services-card-content">{service.content}</p>
-        <a
+        {/* <a
           className="services-link"
           onClick={() => {
             document.querySelector("#UnderConst-wrapper").style.display =
               "flex";
           }}
         >
-          {/* Learn More &gt; */}
-        </a>
+          Learn More &gt;
+        </a> */}
       </div>
     </div>
   );
