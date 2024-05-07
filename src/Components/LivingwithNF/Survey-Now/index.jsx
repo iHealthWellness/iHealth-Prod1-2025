4f0230bf364d242f9ae74f6406f5048e7b000944
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-05-07 21:57:11 +0400

    Living with NF - Made path to nf survey. Changed color of heading in survey page and font of h1

--- src/Components/LivingwithNF/Survey-Now/index.jsx
+++ src/Components/LivingwithNF/Survey-Now/index.jsx
@@ -1,8 +1,16 @@
 import "./index.css";
 import survey from "src/Assets/Images/surveyclinicaltrials.jpg";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function index({ underConstruction }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate("/nf-survey");
+    window.scrollTo(0, 0);
+  };
+
   return (
     <div id="take-survey" className="survey-container">
       <div className="survey-content">
@@ -12,12 +20,12 @@ function index({ underConstruction }) {
           your match in the world of clinical trials!
         </p>
         {/* navigate to "/nf-registry" */}
-        <button className="nf-btn SN-D-T-M-B-16" onClick={underConstruction}>
+        <button className="nf-btn SN-D-T-M-B-16" onClick={handleClick}>
           Take the survey!
         </button>
       </div>
 
-      <img onClick={underConstruction} className="survey-img" src={survey} />
+      <img onClick={handleClick} className="survey-img" src={survey} />
     </div>
   );
 }
