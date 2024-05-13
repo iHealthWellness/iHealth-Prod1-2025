06f93ffa7d3c6248da39268689445161178efe29
AmalStudy <amalengstudy@gmail.com>
2024-05-13 16:30:56 -0700

    Added Ola and Rebecca's cards to Advisory Committe / Fixed  paragraphs

--- src/Components/Team-Layout/DataPage/TeamData.jsx
+++ src/Components/Team-Layout/DataPage/TeamData.jsx
@@ -15,7 +15,14 @@ import Image12 from "src/Assets/Images/Nathalie.png";
 
 const sasikaBio = `Sasika is the founder, Chief Executive Officer (CEO), and Chief Technology Officer (CTO) of iHealth and Wellness Foundation, Inc. --A former Wall Street technologist, she revolutionized enterprise-level web solutions at Prudential Financial, supporting Fixed Income Trading systems & Compliance systems, and PruFN.com with 20 million annual unique visitors. Now, she's disrupting healthcare technology. --At Aetna (a CVS Health Company), Sasika led the implementation of a powerful and interactive claims-driven Personal Health Record (PHR) system, serving 29.9 million individuals. This innovative solution provided members a comprehensive view of their health by integrating detailed, claims-driven information gathered across the health care spectrum - such as physician office, lab, diagnostic treatment and prescriptions. --Passionate about leveraging technology for better healthcare, Sasika established iHealth and Wellness Foundation, Inc. to improve the lives of individuals with complex diseases. Under her leadership, the foundation develops a platform that integrates with major Electronic Health Record (EHR) systems, while offering medical and wellness provider lookup, social networking, and patient advocacy services.--Sasika's contributions have earned her numerous awards in the tech industry and philanthropy, highlighting her dedication to improving lives through innovation. With a unique blend of expertise in technology and a deep understanding of the healthcare landscape, Sasika is dedicated to advancing medical and wellness care through disruptive and innovative solutions. Her visionary approach and commitment to improving lives make her a driving force behind the iHealth and Wellness Foundation's mission.--You'll often find Sasika indulging in her passions for hiking and cycling. Exploring new destinations and immersing herself in live music are also among her favorite pastimes.`;
 
-const bio2 = `Jeff Jacques, MD, is a physician executive leader and entrepreneur who has spent more than 20 years in digital health, building products and services that deliver personalized support for individuals experiencing complex care journeys. As chief medical officer at Virgin Pulse, he focuses on ensuring alignment with member and market needs, enhancing the company’s behavior science approach, and exploring additional ways its unique capabilities can further simplify and support the member journey.--Jeff Co-Founded and served as Chief Executive Officer at CareTribe. He served as Chief Strategy Officer at Cleo. Dr. Jacques joins Virgin Pulse with more than 20 years of experience in digital health, population health management and advanced clinical analytics, developing solutions that support individuals and populations through complex health journeys. He was previously CEO and co-founder of CareTribe, a digital health platform dedicated to supporting the needs of family caregivers. It was acquired by family support company Cleo in 2022, where he became chief strategy officer. He also held leadership roles with CVS Health/Aetna for more than a decade, including as the founder of NeoCare Solutions, a CVS Health/Aetna business supporting the parents of premature infants in the NICU and as chief clinical innovation officer at ActiveHealth Management, a CVS Health company focused on population health. He will also lead the company's Science Advisory Board, which helps ensure that Virgin Pulse's technology and the experiences it supports are rooted in the most relevant scientific evidence and best practices.`;
+
+
+
+const bio2 = `Jeff Jacques, MD, is a physician executive leader and entrepreneur who has spent more than 20 years in digital health, building products and services that deliver personalized support for individuals experiencing complex care journeys. As chief medical officer at Virgin Pulse, he focuses on ensuring alignment with member and market needs, enhancing the company’s behavior science approach, and exploring additional ways its unique capabilities can further simplify and support the member journey.
+-- 
+Jeff Co-Founded and served as Chief Executive Officer at CareTribe. He served as Chief Strategy Officer at Cleo. Dr. Jacques joins Virgin Pulse with more than 20 years of experience in digital health, population health management and advanced clinical analytics, developing solutions that support individuals and populations through complex health journeys. He was previously CEO and co-founder of CareTribe, a digital health platform dedicated to supporting the needs of family caregivers. It was acquired by family support company Cleo in 2022, where he became chief strategy officer. He also held leadership roles with CVS Health/Aetna for more than a decade, including as the founder of NeoCare Solutions, a CVS Health/Aetna business supporting the parents of premature infants in the NICU and as chief clinical innovation officer at ActiveHealth Management, a CVS Health company focused on population health. He will also lead the company's Science Advisory Board, which helps ensure that Virgin Pulse's technology and the experiences it supports are rooted in the most relevant scientific evidence and best practices.`;
+
+
 const bio3 = `Joseph F. Karcich is a highly respected leader known for his genuine concern for those he leads. With a strong background in the financial sector, Joe has established himself as a prominent figure in the industry.--Beginning his career as a management trainee at E.F. Hutton, Joe quickly rose through the ranks, becoming the youngest vice president in the history of the company at the age of 25. This early success laid the foundation for his future accomplishments.--
 Joining Prudential Securities in 1987, Joe faced the challenge of starting his new role immediately after the market crash of that year. Despite the difficult circumstances, his leadership skills shone through. As the Executive Vice President and Director of Global Processing, he played a pivotal role in shaping the success of Prudential Securities and solidifying its position as a leading player in the industry.--
 In addition to his contributions at Prudential, Joe served as the Chairman of the Board of Directors at Wexford Clearing Services LLC, a subsidiary of Wachovia Securities LLC. His expertise and dedication have earned him a reputation as a trusted advisor and influential decision-maker in the financial services arena.--
@@ -61,11 +68,15 @@ With a never-ending mind for curiosity and always trying to find ways to improve
 Currently, after realizing the necessity to strengthen the practice of prevention instead of acute treatment, she follows her passion for helping people achieve their best form through Preventive Medicine and Health Coaching.
 --
 She loves going out in nature, and you can often find her along trails in the mountains of Japan, foraging and enjoying some fresh air with her dog.
+
+
+
+
 `;
 const bio8 = `Christophe Foulon, senior manager and cybersecurity consultant at F10 FinTech, brings over 15 years of experience as a CISO, vCISO, information security manager, adjunct professor, author, and cybersecurity strategist with a passion for customer service, process improvement, and information security. He has also spent over ten years leading, coaching, and mentoring people.
-
+--
 As a security practitioner, Christophe is focused on helping businesses tackle their cybersecurity risks while minimizing friction, resulting in increased resiliency, and helping to secure people and processes with a solid understanding of the technology involved. He gives back by producing a podcast, “Breaking into Cybersecurity,” focused on helping people who are trying to transition into the cybersecurity industry by sharing the stories of those who have done it in the past five years to inspire those looking to do it now. He also co-authored “Develop Your Cybersecurity Career Path: How to Break into Cybersecurity at Any Level” and “Hack the Cybersecurity Interview: A Complete Interview Preparation Guide for jumpstarting your cybersecurity career.”
-
+--
 Christophe holds a Master of Science in Information Technology, Information Assurance, and Cybersecurity, a graduate certificate in Information Systems, and a bachelor's degree in Business Administration/Information Systems from Walden University. These include industry certifications like the CISSP, GSLC, AWS Security Specialization, and more.`;
 const bio9 = ` 
  	
@@ -90,8 +101,10 @@ On the personal side, Mark grew up in Wisconsin, lived in Boston for nearly 20 y
 Mark graduated from the Massachusetts Institute of Technology with a Bachelors and Masters Degree in Electrical Engineering. He also has a Masters in Business Administration from the Harvard Business School.`;
 
 const bio12 = `Michelle Waldgeir is a CPG-trained marketer, operator, and brand builder with broad executive leadership experience across a diverse range of Fortune 500 firms (McKesson, P&G, Clorox), early stage startups, and nonprofits. For decades, she’s been listening intently to customers and developing solutions to better meet their needs and deliver game-changing growth.  
+--
 
 At Clorox, she created and built Clorox Disinfecting Wipes from an idea to a $100M+ business in 1 out of 3 U.S. homes. As Vice President of Consumer Products at McKesson she led a $1B+ marketing and retail services business unit through a major business model transformation. And, as Chief Marketing Officer of a home solar provider she ignited growth that scaled the startup from $100K to $41M over 3 years. The meteoric growth earned Astrum Solar the #2 ranking on Inc. Magazine’s 2012 List of Fastest Growing Private U.S. companies. It was later acquired by Direct Energy.  
+--
 
 A versatile leader, Michelle specializes in helping purpose-driven organizations unlock new possibilities. In her roles as a fractional CMO, she has helped both online marketplaces and health and wellness organizations of all sizes grow exponentially. Michelle earned her B.S. in Economics from The Wharton School of the University of Pennsylvania and her M.B.A. from the Kellogg Graduate School of Management. 
 
@@ -102,12 +115,39 @@ const bio13 = `
  
  	
 Natalie Boehm is a strategic consultant, disability activist, and author who is passionate about creating change. With years of experience in advocacy work, nonprofit, and organizational leadership, Natalie brings a unique blend of skills and knowledge to her role as the president of The Defeating Epilepsy Foundation.
+--
 
 Natalie has dedicated her work to those who are battling chronic illnesses to make sure they get the care they need and have a good quality of life. Natalie has battled epilepsy for the majority of her life and has experienced firsthand the challenges of navigating the healthcare and health insurance industries. 
+--
 
 Throughout her career, Natalie has demonstrated a strong commitment to the mission and vision of her organization, continuously challenging herself to create positive change for those in need. Natalie understands the importance of collaboration, working to bring organizations together to create positive change. 
+--
 
 Natalie wants the next generation of people with chronic illness not to experience the stigma that many from her generation have faced. She is focused on making sure that patients with chronic illnesses are treated with the dignity and respect they deserve despite their challenges.  
+
+
+`;
+
+const bio14 = ` 
+Dr. Rebecca Brown MD PhD is a board-certified adult-focused neurologist and neuro-oncologist who specializes in NF and SWN in addition to primary brain tumors and metastases. She earned her PhD from UT Austin, her MD from UT Southwestern, and completed her neurology residency at Mount Sinai Hospital in 2017. After her neuro-oncology fellowship at Memorial Sloan Kettering Hospital, she served as an instructor and research fellow there for 18-months prior to accepting a position as Assistant Professor and Director of the all-ages NF and SWN Clinic at Mount Sinai (NSCM) in January, 2021. 
+--
+Dr. Brown has been involved in both laboratory research and clinical trials and is particularly interested in how to treat skin bumps associated with NF1 called cutaneous neurofibromas and their effect on mental health. Her other interests are exploring the role of neuro-inflammation in pain from SWN and NF, and developing new therapies for meningioma and malignant peripheral nerve sheath tumors. 
+--
+Dr. Brown lives in NYC with her husband and daughter, and has an interest in art, fashion, and painting.
+
+`;
+
+
+
+const bio15 = ` 
+ 	
+ 	
+Oladimeji Ibitoye is a seasoned back-end developer with over 4 years of experience, specializing in Node.js and JavaScript to craft exceptional user experiences. His expertise extends to designing and implementing software in a test-driven environment, showcasing a knack for tackling challenging projects and transforming innovative ideas into reality through code.
+--
+At Revent Technologies in Lagos, Nigeria, Oladimeji serves as a Senior Backend Developer and Technical Lead, spearheading the development of a Retail Core Banking Solution. His leadership ensures code quality, while his contributions to API development and documentation meet client requirements. Collaborating seamlessly with frontend developers, he enhances integration and consistency, leveraging AWS S3 for file handling, Jest for testing and other backend tools.  Previously, he's contributed to various ventures, including building apps for SMEs for Strich, and collaborating on projects like Zapi, an AI API Marketplace.
+--
+Oladimeji's educational background includes a Bachelor of Science in Civil Engineering from Obafemi Awolowo University, Ile-Ife. With a passion for scalability, performance, and user experience, Oladimeji seeks opportunities to contribute his expertise to challenging projects globally.  
+
 
 
 `;
@@ -206,6 +246,22 @@ const teamData = {
       id: 13,
       name: "Natalie Aswad Boehm",
       extendedBio: bio13,
+      image: Image12,
+      link: "https://www.linkedin.com/in/lizzappi",
+    },
+    {
+      id: 14,
+      name: "Rebecca Brown, MD PhD ",
+      extendedBio: bio14,
+      image: Image11,
+      link: "https://www.linkedin.com/in/lizzappi",
+    },
+
+
+    {
+      id: 15,
+      name: "Oladimeji Ibitoye",
+      extendedBio: bio15,
       image: Image12,
       link: "https://www.linkedin.com/in/lizzappi",
     },
