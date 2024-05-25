45f48c582a982018caeaad057e0b8359ce2a598d
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-05-26 01:52:30 +0400

    changed the content of the main section

--- src/Pages/Survey/index.jsx
+++ src/Pages/Survey/index.jsx
@@ -17,18 +17,24 @@ const index = () => {
       <section className="hero">
         <header className="img-section">
           <div className="hero-content">
-            <video autoPlay muted loop playsInline className="second-img justify-self-right">
+            <video
+              autoPlay
+              muted
+              loop
+              playsInline
+              className="second-img justify-self-right"
+            >
               <source src={surveyVideo} type="video/mp4" />
             </video>
             <h2 className="SN-D-H3-24 visib prt-1 advance-max-width">
               Advance Understanding and Help Others
               <p className="SN-D-H3-36 after-break survey-today">
-              Join a Clinical Trial to Benefit Yourself and Your Community
+                Join a Clinical Trial to Benefit Yourself and Your Community
               </p>
             </h2>
             {/* <h2 className="SN-D-H2-36 invis prt-1"> */}
-              {/* <span className='survey-now'>Survey Today</span> <p> Thrive Tomorrow!</p> */}
-              {/* <p className="SN-D-H1-48 after-break">Change the Future</p> */}
+            {/* <span className='survey-now'>Survey Today</span> <p> Thrive Tomorrow!</p> */}
+            {/* <p className="SN-D-H1-48 after-break">Change the Future</p> */}
             {/* </h2> */}
           </div>
         </header>
@@ -38,11 +44,11 @@ const index = () => {
           Join a Clinical Trial to Benefit Yourself and Your Community
         </h2>
         <p className="SN-D-H3-24 prt-2 clinical-trial-hidden text-align-justify">
-          Completing the survey is just the beginning; the real empowerment lies
-          in participating in active clinical trials. By doing so, you become an
-          integral part of the collective effort to shape innovative healthcare
-          solutions. Your active involvement is the key to a healthier tomorrow
-          for both yourself and the broader community.
+          Are you ready to be an advocate for change? Participate in clinical
+          trials and become an integral part of the scientific breakthroughs
+          shaping tomorrow's treatments. Your participation isn't just a chance
+          to receive advanced medical care — it's an opportunity to shape the
+          landscape of healthcare for generations to come.
         </p>
         {/* onClick={() => setOpenModal(true)} */}
         <button
