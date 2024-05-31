d5de25d7e66d41df65cc8a25f6215f3dc6dea486
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-05-31 15:18:31 +0400

    fix the whole thing

--- src/Constants/HomePage/Benefits.js
+++ src/Constants/HomePage/Benefits.js
@@ -4,29 +4,28 @@ import smileImg from "src/Assets/Images/benefit-smile.jpg";
 const Benefits = [
   {
     image: colleagueImg,
-    title: "Patient Benefit",
+    title: "We help patients",
     summary:
-      "An integrated platform that includes medical and wellness provider lookup, medical and wellness social networking, patient advocacy, and access to all Major EHR Systems can provide several benefits to the patient, including:",
+      "Living with complex conditions should be simple. Since it’s not, we give more power to you, the patient. Personalized to your condition, our all-in-one platform provides the resources and tools you need to make the decisions that best fit your unique health journey.",
     details: [
-      "Easy Access to Healthcare Services",
-      "Enhanced Communication",
-      "Personalized Care",
-      "Improved Health Outcomes",
-      "Time and Cost Savings",
-      "Improved Patient Advocacy",
+      "Gain access to all the services you need",
+      "Learn how to better manage your condition",
+      "Share your health data with doctors, caregivers, and others",
+      "See how your experiences compare with peers",
+      "Grow your support system to live your best life",
     ],
   },
   {
     image: smileImg,
-    title: "Provider Benefit",
+    title: "We help providers too",
     summary:
       "Caring for patients with complex conditions should not be complicated. Personalized to each patient’s condition, our all-in-one platform grows their support system and provides the resources and tools patients need to take control of their own health journey",
     details: [
-      "Increased efficiency",
-      "Improved patient care",
-      "Better patient engagement",
-      "Enhanced patient advocacy",
-      "Competitive advantage",
+      "Connect with the right new patients",
+      "See more engaged and informed patients",
+      "Gain easier access to patient health data",
+      "Provide a better patient experience",
+      "Partner with us to grow your practice",
     ],
   },
 ];
