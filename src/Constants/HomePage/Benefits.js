75599ff461dac005bd63d34e38cc44453c6ce6c4
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-05-31 15:09:36 +0400

    update benefit card - switch text

--- src/Constants/HomePage/Benefits.js
+++ src/Constants/HomePage/Benefits.js
@@ -1,32 +1,32 @@
-import colleagueImg from "src/Assets/Images/benefit-cheers.jpg";
+import colleagueImg from "src/Assets/Images/benefit-colleague.jpg";
 import smileImg from "src/Assets/Images/benefit-smile.jpg";
 
 const Benefits = [
   {
     image: colleagueImg,
-    title: "We help patients",
+    title: "Patient Benefit",
     summary:
-      "Living with complex conditions should be simple. Since it’s not, we give more power to you, the patient. Personalized to your condition, our all-in-one platform provides the resources and tools you need to make the decisions that best fit your unique health journey.",
+      "An integrated platform that includes medical and wellness provider lookup, medical and wellness social networking, patient advocacy, and access to all Major EHR Systems can provide several benefits to the patient, including:",
     details: [
-      "Gain access to all the services you need",
-      "Learn how to better manage your condition",
-      "Share your health data with doctors, caregivers, and others",
-      "See how your experiences compare with peers",
-      "Grow your support system to live your best life",
+      "Easy Access to Healthcare Services",
+      "Enhanced Communication",
+      "Personalized Care",
+      "Improved Health Outcomes",
+      "Time and Cost Savings",
+      "Improved Patient Advocacy",
     ],
   },
   {
     image: smileImg,
-    title: "We help providers too",
+    title: "Provider Benefit",
     summary:
-      "Caring for patients with complex conditions should not be complicated. Personalized to each patient’s condition, our all-in-one platform grows their support system and provides the resources and tools patients need to take control of their own health journey.",
+      "An integrated platform that includes medical and wellness provider lookup, medical and wellness social networking, patient advocacy, and access to all Major EHR Systems can provide several benefits to the provider, including:",
     details: [
-      "Connect with the right new patients",
-      "See more engaged and informed patients",
-      "Gain easier access to patient health data",
-      // "Improved Health Outcomes",
-      "Provide a better patient experience",
-      "Partner with us to grow your practice",
+      "Increased efficiency",
+      "Improved patient care",
+      "Better patient engagement",
+      "Enhanced patient advocacy",
+      "Competitive advantage",
     ],
   },
 ];
