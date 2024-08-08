a838eccbaab8ab0524d4a8f01bc0cf78877c7db7
DrTevzadze <dr.davidtevzadze@gmail.com>
2024-08-09 00:13:15 +0400

    add instragram icon

--- src/Constants/HomePage/SocialMedia.js
+++ src/Constants/HomePage/SocialMedia.js
@@ -14,7 +14,10 @@ const SocialMedia = [
     url: "https://www.linkedin.com/company/ihealth-and-wellness-foundation-inc/posts/?feedView=all&viewAsMember=true&lipi=urn%3Ali%3Apage%3Acompanies_company_posts_index%3B3e516723-94c6-4b10-98d6-b80aebe129ef",
   },
   // { icon: TwitterIcon, url: "#" },
-  // { icon: InstagramIcon, url: "#" },
+  {
+    icon: InstagramIcon,
+    url: "https://www.instagram.com/ihealth.wellness.foundation?igsh=MW52bmR0cHM5eHZ0dw%3D%3D&utm_source=qr",
+  },
   // { icon: YoutubeIcon, url: "#" },
 ];
 
