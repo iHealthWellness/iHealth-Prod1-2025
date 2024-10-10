0fa2e0309dff69496feb65e59b1e0b05440d014e
Sasika Nanayakkara <onehealthconnectapp@gmail.com>
2024-10-10 13:55:20 -0400

    deleted previous Dev1 structure

--- src/config/urls.js
+++
@@ -1,3 +0,0 @@
-import { BASE_URL } from '../environment/config';
-
-export const getProviders = `${BASE_URL}/api/v1/646a366608cb638735093bc7`;
--- src/utils/donateLink.js
+++
@@ -1,8 +0,0 @@
-const donateLink = () => {
-  window.open(
-    "https://www.paypal.com/donate/?hosted_button_id=VE7E4C2UBA3EN",
-    "_blank"
-  );
-};
-
-export default donateLink;
