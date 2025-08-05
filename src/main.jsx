bad3429a8b993c09656ab61fbf0f0e60aa225c87
Sasika Nanayakkara <onehealthconnectapp@gmail.com>
2025-08-05 12:37:58 -0400

    8-5-25-Brian-SN-Deploying-Landing Page - Mobile Wrapper Security Component #113

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,19 @@
 //Import Libraries/Packages
 import React from "react";
 import ReactDOM from "react-dom/client";
-
-
-
+// import { store } from "./0-Dev1-General/UserAuthentication/redux-store/store";
+import { Provider } from 'react-redux'
 
 //Import Local files/components
 import App from "./App.jsx";
-
-
-
+import { SecurityPrechecks } from './Components/Security-Prechecks/SecurityPrechecks.jsx';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <SecurityPrechecks>
+      {/* <Provider store={store}> */}
+        <App />
+      {/* </Provider> */}
+    </SecurityPrechecks>
   </React.StrictMode>
 );
